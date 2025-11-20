47bf31b7ce50b902162da7da8c01705b2275ad64
nxxgxr <167641376+nxxgxr@users.noreply.github.com>
2025-11-20 12:23:50 +0200

    Update 16317.py

--- task-6-cherepaha/16317.py
+++ task-6-cherepaha/16317.py
@@ -1,37 +1,30 @@
-from turtle import *
-tracer(False)
-
-screensize(4000,4000)
-m=20
+from functools import *
 
 
-for i in range(2):
-    fd(21*m)
-    rt(90)
-    fd(27*m)
-    rt(90)
+def H(p): return p + 1, p * 2
 
-up()
-fd(9*m)
-rt(90)
-fd(10*m)
-lt(90)
 
-down()
-for i in range(2):
-    fd(86*m)
-    rt(90)
-    fd(47*m)
-    rt(90)
+@lru_cache(None)
+def f(p):
+    if p >= 129: return 'L'
+    return 'W' if any(f(h) == 'L' for h in H(p)) else 'L'
 
-up()
-for x in range(-25,50):
-    for y in range(-35,15):
-        goto(x*m,y*m)
-        dot(3,'red')
+
+@lru_cache(None)
+def c(p):
+    if p >= 129: return 0
+    if f(p) == 'L': return 1 + max(c(h) for h in H(p))
+    return 1 + min(c(h) for h in H(p) if f(h) == 'L')
+
+
+# for s in range(1, 129):
+#     if c(s) < 5: print(s, f(s), c(s))
+
+print('19a:', min(s for s in range(1, 129) if c(s) == 2))
+print('19б:', min(s for s in range(1, 129) if c(s * 2) == 1))
+print('19в:', min(s for s in range(1, 129) if c(s) == 1))
 
 
 
 
-update()
-done()
+
