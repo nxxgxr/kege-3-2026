e513344091cc3e4d79eb51215ed90b235ea64517
Администратор <egorkozakov420@gmail.com>
2025-12-11 21:54:04 +0200

    дз

--- task-8/6261.py
+++ task-8/6261.py
@@ -5,7 +5,7 @@ for val in product(printable[:8],repeat = 10):
     val=''.join(val)
     if val[0] != '0' and  val.count('7') == 5:
         for i in range(len(val)-1):
-            if int(val[i]) % 2 == 1 and val[i+1] == '7':
+            if (int(val[i]) % 2 == 1 and val[i+1] == '7') or (val[i] == '7' and int(val[i+1]) % 2 ==1):
                 break
         else:
             cnt+=1
