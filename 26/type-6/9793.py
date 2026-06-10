87b8f98bfb888b5df38f1ca7db32090f24349af2
s3pt1ck <egorkozakov420@gmail.com>
2026-06-10 14:50:22 +0200

    классная работа

--- 26/type-6/9793.py
+++ 26/type-6/9793.py
@@ -25,3 +25,26 @@ for piece in pieces:
     last_piece = piece
 
 print(last_piece[2], cnt - 1 if last_piece[1] == 's' else 0)
+
+
+
+
+####################################################
+
+
+with open(r'..\files\26_9793.txt') as file:
+    N = int(file.readline())
+    pieces = []
+    for pos, data in enumerate(file, start=1):
+        time_1, time_2 = map(int, data.split())
+        if min(time_1, time_2) == time_1:
+            pieces.append([time_1, 's', pos])
+        else:
+            pieces.append([time_2, 'o', pos])
+
+pieces = sorted(pieces)
+
+last_piece = pieces[-1]
+cnt = sum(1 for piece in pieces if piece[1] == 's')
+
+print(last_piece[2], cnt - 1 if last_piece[1] == 's' else 0)
